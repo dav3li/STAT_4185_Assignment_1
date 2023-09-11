697691970827b8f356b875064cdc6ffee320f9b5
David Li <dav3li@MacBook-Pro-3.local>
2023-09-11 14:10:01 -0400

    decryt 2 solved

--- Git_Github_HW/decrypt_message_two.py
+++ Git_Github_HW/decrypt_message_two.py
@@ -1,7 +1,23 @@
-encrypted_file = open("encrypted_message_two.txt", 'r')
 
-encrypted_message = encrypted_file.readline()
-
-encrypted_file.close()
 
 # Write Code Here
+def decrypt2(file):
+    encrypted_file = open(file, 'r')
+
+    encrypted_message = encrypted_file.readline().strip()
+    print(encrypted_message)
+    encrypted_file.close()
+    encrypted_message = list(encrypted_message)
+    len_encrypted_message = len(encrypted_message)
+    index_start = 1 #encrypted_message
+    index_end = len_encrypted_message - 2
+    print(index_start, index_end, len_encrypted_message)
+    while index_start < index_end:
+        encrypted_message[index_start], encrypted_message[index_end] = encrypted_message[index_end], encrypted_message[index_start]
+        index_start += 2
+        index_end -= 2
+
+    return "".join(encrypted_message)
+
+print(decrypt2("./Git_Github_HW/encrypted_message_two.txt"))
+
